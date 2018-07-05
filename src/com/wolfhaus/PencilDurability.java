6271a461ac4f458d1d3cb1f537bc482f7f1a8828
swolfenb <swolfenbarger@bodhiserver.info>
2018-07-05 16:36:22 -0400

    Added property pencilDurability to PencilDurabilityTest. Updated testPencilWriteSeaShellsWithDurability to include enough durability to successfully pass. Updated method write to check each letter before writing to determine if the letter or a " " should be written on the paper.

--- src/com/wolfhaus/PencilDurability.java
+++ src/com/wolfhaus/PencilDurability.java
@@ -16,13 +16,54 @@ public class PencilDurability {
     /**
      * The paper the pencil will write text on.
      */
-    public String paper;
+    public String paper = "";
+
+    /**
+     * The durability of the pencil point.
+     */
+    public int pointDurability;
+
+    /**
+     * Constructor Method for a pencil
+     * @param pointDurability int containing the point durability.
+     */
+    public PencilDurability(int pointDurability) {
+        this.pointDurability = pointDurability;
+    }
 
     /**
      * Method to write text on a paper
      * @param text string containing text to be written to the paper.
      */
     public void write(String text) {
-        this.paper = text;
+        // Check durability before writing the letter to the paper.
+        for (int i = 0; i < text.length(); i++) {
+            // Determine if this letter is upper case
+            boolean isUpperCase = Character.isUpperCase(text.charAt(i));
+
+            if(isUpperCase) {
+                if (this.pointDurability >= 2) {
+                    // Append the next letter.
+                    this.paper += text.charAt(i);
+
+                    // Deduct two points of durability.
+                    this.pointDurability -= 2;
+                } else {
+                    // Append a space.
+                    this.paper += ' ';
+                }
+            } else {
+                if (this.pointDurability >= 1) {
+                    // Append the next letter.
+                    this.paper += text.charAt(i);
+
+                    // Deduct one points of durability.
+                    this.pointDurability -= 1;
+                } else {
+                    // Append a space.
+                    this.paper += ' ';
+                }
+            }
+        }
     }
 }
--- test/com/wolfhaus/PencilDurabilityTest.java
+++ test/com/wolfhaus/PencilDurabilityTest.java
@@ -1,5 +1,6 @@
 package com.wolfhaus;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,21 +18,44 @@ import static org.junit.Assert.assertEquals;
 public class PencilDurabilityTest {
 
     /**
+     * The VendingMachine object to be used with the tests.
+     */
+    private PencilDurability pencilDurability;
+
+    /**
      * Test to identify the text written on a paper is equal to the instructed
-     * text to write.
+     * text to write. This pencil has enough durability.
      */
     @Test
-    public void testPencilWriteSeaShells() {
-        PencilDurability pencilDurability = new PencilDurability();
+    public void testPencilWriteSeaShellsWithDurability() {
+        // Instantiate a new pencil with 5000 point durability.
+        this.pencilDurability = new PencilDurability(5000);
 
         // Sample text to write
         String text = "She sells sea shells sea shells down by the sea shore";
 
         // Instruct the pencil to write the sample text.
-        pencilDurability.write(text);
+        this.pencilDurability.write(text);
 
         // The sample text should be the only text currently on the paper.
-        assertEquals(text, pencilDurability.paper);
+        assertEquals(text, this.pencilDurability.paper);
+    }
 
+    /**
+     * Try to write "Text" with one point durability short.
+     */
+    @Test
+    public void testPencilWriteTextWithoutFullDurability() {
+        // Instantiate a new pencil with 5 point durability.
+        this.pencilDurability = new PencilDurability(4);
+
+        // Sample text to write
+        String text = "Text";
+
+        // Instruct the pencil to write the sample text.
+        this.pencilDurability.write(text);
+
+        // The sample text should be the only text currently on the paper.
+        assertEquals("Tex ", this.pencilDurability.paper);
     }
 }
