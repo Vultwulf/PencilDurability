bb515d4779d3da93a59dc15a283fdcc24f06ef76
swolfenb <swolfenbarger@bodhiserver.info>
2018-07-05 15:44:49 -0400

    Initial commit

---
+++ src/com/wolfhaus/PencilDurability.java
@@ -0,0 +1,15 @@
+package com.wolfhaus;
+
+/**
+ * The PencilDurability program implements an application that
+ * satisfies the requirements of the kata-pencil-durability
+ * project.
+ *
+ * https://github.com/PillarTechnology/kata-pencil-durability
+ *
+ * @author  Scott Wolfenbarger
+ * @version 1.0
+ * @since   2018-07-05
+ */
+public class PencilDurability {
+}
---
+++ test/com/wolfhaus/PencilDurabilityTest.java
@@ -0,0 +1,14 @@
+package com.wolfhaus;
+
+/**
+ * The PencilDurabilityTest implements all test-cases for use
+ * of Test Driven Development for the Pencil Durability program.
+ *
+ * https://github.com/PillarTechnology/kata-pencil-durability
+ *
+ * @author  Scott Wolfenbarger
+ * @version 1.0
+ * @since   2018-07-05
+ */
+public class PencilDurabilityTest {
+}
